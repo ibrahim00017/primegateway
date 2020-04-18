538bc2f7be2b2961d2fe088ab26c68c0237bac29
Ibrahim ABLADON <hibrahimyya@gmail.com>
2020-04-18 15:55:31 +0100

    Initial version of primegateway generated by JHipster-6.7.1

---
+++ src/test/java/com/mpd/prime/cucumber/CucumberContextConfiguration.java
@@ -0,0 +1,20 @@
+package com.mpd.prime.cucumber;
+
+import com.mpd.prime.PrimegatewayApp;
+import io.cucumber.java.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+@ContextConfiguration(classes = PrimegatewayApp.class)
+public class CucumberContextConfiguration {
+
+    @Before
+    public void setup_cucumber_spring_context() {
+        // Dummy method so cucumber will recognize this class as glue
+        // and use its context configuration.
+    }
+
+}
